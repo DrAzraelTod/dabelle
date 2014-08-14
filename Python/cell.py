4d967f403d0fef83c2e993f726833c65a9920f2f
Dr. Azrael Tod <code@g33ky.de>
2014-08-14 14:22:05 +0200

    parsing properties now works for unclosed ones, example-file

--- Python/cell.py
+++ Python/cell.py
@@ -11,21 +11,27 @@ class Cell(object):
     self.y = y
     while True:
       temp = self.search_property(substr)
-      if temp == substr:
+      if len(temp) >= len(substr):
         break
       substr = temp
     self.value = substr
     print("cell %i %i - %s" % (self.x, self.y, self.value))
 
   def search_property(self,substr):
+    props = "_!#&="
+    print("search %s" % substr)
     r = re.compile(r"^([^a-zA-Z0-9])(.*)(\1)")
     res = r.split(substr)
     if len(res) > 3:
       # "_multiply with_!titles" becomes:
       # ['', '_', 'multiply with', '_', '!titles']
-      self.found_property(res[1],res[2])
+      if res[1] in props:
+        self.found_property(res[1],res[2])
       return res[-1]
     else:
+      if substr[0] in props:
+        self.found_property(substr[0],substr[1:])
+        return substr[1:]
       return substr
 
   def found_property(self,name,value):
